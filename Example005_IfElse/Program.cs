fe3a8382f9910d329d9e463aa6b80f665e45e4a3
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-14 18:00:10 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Ура, это НАТАША!");
+}
+else
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
